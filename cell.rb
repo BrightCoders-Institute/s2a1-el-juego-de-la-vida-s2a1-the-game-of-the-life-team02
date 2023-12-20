29caf440424ae6d563b4dcfb1e51aac2077f06d7
Fernando Arellano <farellano.0709@gmail.com>
2023-12-20 17:34:43 -0600

    "Modified Comments"cd '/home/farellano/Documentos/BrightCoders/s2a1-el-juego-de-la-vida-s2a1-the-game-of-the-life-team02'
    Co-authored-by: ramondiaz21 <jramondiaz96@gmail.com>
    Co-authored-by: yhoselin  <yossi2075.ys@gmail.com>

--- cell.rb
+++ cell.rb
@@ -2,15 +2,13 @@
 
 # Clase Celular para definir los vivos y muertos del juego
 class Cell
-  attr_accessor :status
-  ## Estado para saber si esta viva o muerta
-  ## * == vivo  . == muerto
+  attr_accessor :status # Viva o muerto
 
   def initialize
     @status = rand(2)
   end
 
   def mostrar
-    @status == 1 ? '*' : '.'
+    @status == 1 ? '*' : '.' # * = Vivo | . = Muerto
   end
 end
