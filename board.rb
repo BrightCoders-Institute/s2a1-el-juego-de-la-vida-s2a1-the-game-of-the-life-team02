b5579a11a1ca444c3d5d067d077c0e15e7221f1a
Fernando Arellano <farellano.0709@gmail.com>
2023-12-20 18:16:12 -0600

    Co-authored-by: Ramon Diaz <jramondiaz96@gmail.com> Co-authored-by: Yhoselin Sanchez <yossi2075.ys@gmail.com>

--- board.rb
+++ board.rb
@@ -16,9 +16,45 @@ class Board
       @columns.times do |j|
         print @grid[i][j].mostrar
       end
-      puts # Agregar un salto de línea después de cada fila
+      puts # Agregar un salto de linea después de cada fila
     end
   end
 
-  attr_reader :grid
+  def count_neighbor
+    @rows.times do |i|
+      @columns.times do |j|
+        count = count_surrounding_cells(i, j)
+        print count.to_s
+      end
+      puts # Agregar un salto de linea después de cada fila
+    end
+  end
+
+  # Metodo para contar las células vivas y muertas en las posiciones circundantes a [i][j]
+  def count_surrounding_cells(i, j)
+    live_count = 0
+    dead_count = 0
+
+    # Bucle para recorrer las posiciones circundantes
+    (-1..1).each do |row_offset|
+      (-1..1).each do |col_offset|
+        # Calcular las coordenadas de la posicion circundante
+        row = i + row_offset
+        col = j + col_offset
+
+        # Verificar que la posicion circundante esté dentro de los límites del tablero
+        if row >= 0 && row < @rows && col >= 0 && col < @columns
+          # Incrementar el contador correspondiente segun el estado de la célula en la posición circundante
+          if @grid[row][col].status == 1
+            live_count += 1
+          else
+            dead_count += 1
+          end
+        end
+      end
+    end
+
+    # Retornar un hash con los recuentos
+    { live: live_count, dead: dead_count }
+  end
 end
